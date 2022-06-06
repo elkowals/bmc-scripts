e54ebb405a9568d5061a6f926920da21c8d209cd
elkowals <elkowalski@berkeley.edu>
2022-06-06 17:17:24 -0500

    Update OpenBMC_conditional_statements.py

--- openBMC/OpenBMC_conditional_statements.py
+++ openBMC/OpenBMC_conditional_statements.py
@@ -6,10 +6,35 @@ import re
 import datetime
 import time
 
-
+apss_id="none"
+ucd_id="none"
 ps_string=os.popen("ps | grep apss_readings.py").read()
 split_ps=ps_string.split("\n")
 for s in split_ps:
     if "python" in s:
-        print(s.split(" ")[0])
-        print("here")
+        apss_id=s.split(" ")[0]
+
+ps_string=os.popen("ps | grep ucd_readings.py").read()
+split_ps=ps_string.split("\n")
+for s in split_ps:
+    if "python" in s:
+        ucd_id=s.split(" ")[0]
+
+
+pins=sys.argv[1].split(",");
+gpiochip=os.popen("gpiodetect | grep ucd").read()
+gpiochip=gpiochip.split(" ")[0]
+
+
+for pin in pins:
+    state=os.popen("gpioget "+gpiochip+" "+pin).read()
+    state=re.sub(r"[\n\t\s]*", "", state)
+    if state == "0":
+        print("pgood fail on "+pin)
+        sleep(60)
+        if apss_id !="none" :
+            os.system("kill "+apss_id)
+        if ucd_id !="none" :
+            os.system("kill "+ucd_id)
+        exit()
+
