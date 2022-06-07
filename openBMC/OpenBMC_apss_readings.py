6d7844aa024099e7dca8a76ce9d454d35ff7179e
elkowals <elkowalski@berkeley.edu>
2022-06-07 09:31:28 -0500

    Update OpenBMC_apss_readings.py

--- openBMC/OpenBMC_apss_readings.py
+++ openBMC/OpenBMC_apss_readings.py
@@ -201,6 +201,6 @@ else:
                     totalPower+=(conversion*12)
                 print("CH"+str(INDEX)+"- "+CHANNELS[INDEX]+" *"+str(conversion)+"* Amps | Power = *"+str(totalPower)+"* W at *"+date_time+"* \n")
                 file.write("CH"+str(INDEX)+"- "+CHANNELS[INDEX]+" *"+str(conversion)+"* Amps | Power = *"+str(totalPower)+"* W at *"+date_time+"* \n")
-        if (time.time() - last_time) >=3600:
+        if (time.time() - last_time) >=2:
             file.truncate(0)
             last_time=time.time()
