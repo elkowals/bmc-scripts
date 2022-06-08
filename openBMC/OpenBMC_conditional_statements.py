4a81279231a6eb6ac0100b7faa2766fe19b7810f
elkowals <elkowalski@berkeley.edu>
2022-06-07 22:41:12 -0500

    Update OpenBMC_conditional_statements.py

--- openBMC/OpenBMC_conditional_statements.py
+++ openBMC/OpenBMC_conditional_statements.py
@@ -8,33 +8,38 @@ import time
 
 apss_id="none"
 ucd_id="none"
-ps_string=os.popen("ps | grep apss_readings.py").read()
+ps_string=os.popen("ps | grep apss_read").read()
+if ps_string[0] == ' ':
+    ps_string=ps_string[1:]
 split_ps=ps_string.split("\n")
 for s in split_ps:
-    if "python" in s:
+    if "apss_readings" in s:
         apss_id=s.split(" ")[0]
 
-ps_string=os.popen("ps | grep ucd_readings.py").read()
+ps_string=os.popen("ps | grep ucd_read").read()
+if ps_string[0] == ' ':
+    ps_string=ps_string[1:]
 split_ps=ps_string.split("\n")
 for s in split_ps:
-    if "python" in s:
+    if "ucd_readings" in s:
         ucd_id=s.split(" ")[0]
 
+print(apss_id)
 
 pins=sys.argv[1].split(",");
 gpiochip=os.popen("gpiodetect | grep ucd").read()
 gpiochip=gpiochip.split(" ")[0]
-
-
-for pin in pins:
-    state=os.popen("gpioget "+gpiochip+" "+pin).read()
-    state=re.sub(r"[\n\t\s]*", "", state)
-    if state == "0":
-        print("pgood fail on "+pin)
-        sleep(60)
-        if apss_id !="none" :
-            os.system("kill "+apss_id)
-        if ucd_id !="none" :
-            os.system("kill "+ucd_id)
-        exit()
-
+pins_values=sys.argv[2].split(",");
+while True:
+    for index in range(len(pins)):
+        state=os.popen("gpioget "+gpiochip+" "+pins[index]).read()
+        state=re.sub(r"[\n\t\s]*", "", state)
+        if state == pins_values[index]:
+            printedValue="echo \"Power-Telemetry triggered: index = "+pins[index]+" and value that triggered = "+pins_values[index]+"\" | systemd-cat";
+            os.system(printedValue)
+            time.sleep(1)
+            if apss_id !="none" :
+                os.system("kill "+apss_id)
+            if ucd_id !="none" :
+                os.system("kill "+ucd_id)
+            exit()
